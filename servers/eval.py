46fd9d505b4194397128af87eaf893a05c9aba20
Aleksey Bilogur <aleksey@residentmar.io>
2020-11-17 20:02:24 -0500

    Fixes.

--- servers/eval.py
+++ servers/eval.py
@@ -223,3 +223,6 @@ def main():
     start_time = time.time()
     eval_fn(dataloader, model, device)
     print(f"Evaluation done in {str(time.time() - start_time)} seconds.")
+
+if __name__ == "__main__":
+    main()
