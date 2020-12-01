6ce809f41e0266fb6e9e0960ac9695689dbefaad
Aleksey Bilogur <aleksey@residentmar.io>
2020-12-01 14:13:42 -0500

    Attempt.

--- servers/server.py
+++ servers/server.py
@@ -1,12 +1,4 @@
 import torch
-# import numpy as np
-# from PIL import Image
-
-# import os
-# import yaml
-# import io
-# import base64
-# import json
 
 from spell.serving import BasePredictor
 
@@ -19,4 +11,40 @@ class Predictor(BasePredictor):
         self.model = torch.load(f"/model/model.pth", map_location=self.device)
 
     def predict(self, payload):
-        return payload
+        payload = {'attention_mask': 
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0],
+        'input_ids':
+            [101, 8699, 102, 1045, 1036, 1040, 2031, 5838, 1010, 2065, 1045,
+             2020, 2183, 102, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0],
+        'token_type_ids':
+            [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0],
+        'y_first': 4,
+        'y_last': 13}
+        input_ids = torch.tensor([payload['input_ids']]).to(self.device)
+        token_type_ids = torch.tensor([payload['token_type_ids']]).to(self.device)
+        attention_mask = torch.tensor([payload['attention_mask']]).to(self.device)
+        start_logits, end_logits = self.model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids
+        )
+        start_logits = start_logits.detach().tolist()
+        end_logits = start_logits.detach().tolist()
+        return {"start_logits": start_logits, "end_logits": end_logits}
