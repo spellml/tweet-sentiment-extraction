172f6982ea6d192a7353e1a84f3f742039652aae
Aleksey Bilogur <aleksey@residentmar.io>
2021-01-11 18:38:19 -0500

    Pathing...

--- scripts/update_model_server.sh
+++ scripts/update_model_server.sh
@@ -2,5 +2,5 @@
 # This script performs a rolling update of a Spell model server.
 set -ex
 
-ls $PWD/tweet-sentiment-extraction/servers/
-spell server update --from-file $PWD/tweet-sentiment-extraction/servers/config.yaml
+ls $PWD/servers/
+spell server update --from-file $PWD/servers/config.yaml
